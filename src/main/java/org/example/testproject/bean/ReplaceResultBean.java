d98ae003d8d064ae9048164e04a0cd60e9017231
chf999 <chenhaifeng999@126.com>
2025-05-21 00:14:44 +0800

    add

---
+++ src/main/java/org/example/testproject/bean/ReplaceResultBean.java
@@ -0,0 +1,10 @@
+package org.example.testproject.bean;
+
+import lombok.Data;
+
+@Data
+public class ReplaceResultBean {
+    private String outPutStr;
+    private String replaceSourceStr;
+    private String replaceTargetStr;
+}
